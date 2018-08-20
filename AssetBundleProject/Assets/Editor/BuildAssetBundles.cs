8c12cec782c4481125035325270745473dbaf802
HokChen <34539753+HokChen@users.noreply.github.com>
2018-08-20 21:53:11 +0800

    AssetBundleProject

---
+++ AssetBundleProject/Assets/Editor/BuildAssetBundles.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class BuildAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundle")]
+    private static void BuildBundles()
+    {
+        string path = "AssetBundles";
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
---
+++ AssetBundleProject/Assets/Scripts/LoadFromFileExample.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadFromFileExample : MonoBehaviour
+{
+
+    private IEnumerator Start()
+    {
+        //AssetBundle ab = AssetBundle.LoadFromFile("AssetBundles/wall.unity3d");
+        //while (!Caching.ready)
+        //{
+        //    yield return null;
+        //}
+
+        //WWW www = WWW.LoadFromCacheOrDownload("http://localhost/AssetBundles/wall.unity3d", 1);
+        //yield return www;
+        //if (!string.IsNullOrEmpty(www.error))
+        //{
+        //    Debug.Log(www.error);
+        //    yield return null;
+        //}
+        //AssetBundle ab= www.assetBundle;
+
+        string url = "http://localhost/AssetBundles/Cube.unity3d";
+        string url2 = "http://localhost/AssetBundles/texture.unity3d";
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(url);
+        UnityWebRequest request2 = UnityWebRequestAssetBundle.GetAssetBundle(url2);
+        yield return request.SendWebRequest();
+        yield return request2.SendWebRequest();
+
+        AssetBundle ab = (request.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+        AssetBundle abMaterial = (request2.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+        GameObject wallGo = ab.LoadAsset<GameObject>("Cube");
+        //GameObject maGo = ab.LoadAsset<GameObject>("Hand_Painted_Grass");
+        GameObject.Instantiate(wallGo);
+
+
+        #region 获取AssetBundle下的Manifest文件并获取里面的依赖关系
+        string url3 = "http://localhost/AssetBundles/AssetBundles";
+        UnityWebRequest request3= UnityWebRequestAssetBundle.GetAssetBundle(url3);
+        yield return request3.SendWebRequest();
+        AssetBundle manifest = (request3.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+        AssetBundleManifest manifest2= manifest.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+        string[] bundles= manifest2.GetAllAssetBundles();
+        string[] dependon = manifest2.GetAllDependencies("cube.unity3d");
+        foreach (var item in bundles)
+        {
+            Debug.Log(item);
+        }
+
+        foreach (var item in dependon)
+        {
+            Debug.Log(item);
+        }
+        #endregion
+
+    }
+}
